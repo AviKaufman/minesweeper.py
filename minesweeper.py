31e1c3b55f58778ba0c2075740ff6d182288401f
AviKaufman <sarkavism@gmail.com>
2022-11-14 15:43:15 -0600

    space bar

--- minesweeper.py
+++ minesweeper.py
@@ -13,6 +13,9 @@ class Globals ():
     mines = 99
     game_frame = None
     board = None
+    x = 0
+    y = 0
+
 
 
 
@@ -28,14 +31,16 @@ class GameBoard(Frame):
         self.width = width
         self.mines = mines
 
+        self.gamewon = None
+
         #create a list of mine values
 
-        mine_grid = [[0]*width for i in [0]*height]
+        self.mine_grid = [[0]*width for i in [0]*height]
         while mines > 0:
             i = random.randint(0,height-1)
             j = random.randint(0,width-1)
-            if mine_grid[i][j] == 0:
-                mine_grid[i][j] = 1
+            if self.mine_grid[i][j] == 0:
+                self.mine_grid[i][j] = 1
                 mines -= 1
 
         #create the board
@@ -44,36 +49,37 @@ class GameBoard(Frame):
 
         for i in range(height):
             for j in range(width):
-                if mine_grid[i][j] == 1:
+                if self.mine_grid[i][j] == 1:
                     self.tiles[i,j] = Tile(self.game,i,j,True,NONE)
                 else:
                     mine_neighbors = 0     #counting nearby mines if Tile in creation is not a mine
                     if i - 1 >= 0:
-                        if mine_grid[i-1][j] == 1:
+                        if self.mine_grid[i-1][j] == 1:
                             mine_neighbors += 1
                     if i - 1 >= 0 and j - 1 >= 0:
-                        if mine_grid[i-1][j-1] == 1:
+                        if self.mine_grid[i-1][j-1] == 1:
                             mine_neighbors += 1
                     if i - 1 >= 0 and j + 1 < width:
-                        if mine_grid[i-1][j+1] == 1:
+                        if self.mine_grid[i-1][j+1] == 1:
                             mine_neighbors += 1
                     if j - 1 >= 0:
-                        if mine_grid[i][j-1] == 1:
+                        if self.mine_grid[i][j-1] == 1:
                             mine_neighbors += 1
                     if j + 1 < width:
-                        if mine_grid[i][j+1] == 1:
+                        if self.mine_grid[i][j+1] == 1:
                             mine_neighbors += 1
                     if i + 1 < height:
-                        if mine_grid[i+1][j] == 1:
+                        if self.mine_grid[i+1][j] == 1:
                             mine_neighbors += 1
                     if i + 1 < height and j - 1 >= 0:
-                        if mine_grid[i+1][j-1] == 1:
+                        if self.mine_grid[i+1][j-1] == 1:
                             mine_neighbors += 1
                     if i + 1 < height and j + 1 < width:
-                        if mine_grid[i+1][j+1] == 1:
+                        if self.mine_grid[i+1][j+1] == 1:
                             mine_neighbors += 1
                     
                     self.tiles[i, j] = Tile(self.game, i, j, False, mine_neighbors)
+
 
     #update the board when game mode is changed
 
@@ -90,12 +96,12 @@ class GameBoard(Frame):
         self.mines = mines
         #create a list of mine values
 
-        mine_grid = [[0]*width for i in [0]*height]
+        self.mine_grid = [[0]*width for i in [0]*height]
         while mines > 0:
             i = random.randint(0,height-1)
             j = random.randint(0,width-1)
-            if mine_grid[i][j] == 0:
-                mine_grid[i][j] = 1
+            if self.mine_grid[i][j] == 0:
+                self.mine_grid[i][j] = 1
                 mines -= 1
 
         #create the board
@@ -105,36 +111,51 @@ class GameBoard(Frame):
 
         for i in range(height):
             for j in range(width):
-                if mine_grid[i][j] == 1:
+                if self.mine_grid[i][j] == 1:
                     self.tiles[i,j] = Tile(self.game,i,j,True,NONE)
                 else:
                     mine_neighbors = 0     #counting nearby mines if Tile in creation is not a mine
                     if i - 1 >= 0:
-                        if mine_grid[i-1][j] == 1:
+                        if self.mine_grid[i-1][j] == 1:
                             mine_neighbors += 1
                     if i - 1 >= 0 and j - 1 >= 0:
-                        if mine_grid[i-1][j-1] == 1:
+                        if self.mine_grid[i-1][j-1] == 1:
                             mine_neighbors += 1
                     if i - 1 >= 0 and j + 1 < width:
-                        if mine_grid[i-1][j+1] == 1:
+                        if self.mine_grid[i-1][j+1] == 1:
                             mine_neighbors += 1
                     if j - 1 >= 0:
-                        if mine_grid[i][j-1] == 1:
+                        if self.mine_grid[i][j-1] == 1:
                             mine_neighbors += 1
                     if j + 1 < width:
-                        if mine_grid[i][j+1] == 1:
+                        if self.mine_grid[i][j+1] == 1:
                             mine_neighbors += 1
                     if i + 1 < height:
-                        if mine_grid[i+1][j] == 1:
+                        if self.mine_grid[i+1][j] == 1:
                             mine_neighbors += 1
                     if i + 1 < height and j - 1 >= 0:
-                        if mine_grid[i+1][j-1] == 1:
+                        if self.mine_grid[i+1][j-1] == 1:
                             mine_neighbors += 1
                     if i + 1 < height and j + 1 < width:
-                        if mine_grid[i+1][j+1] == 1:
+                        if self.mine_grid[i+1][j+1] == 1:
                             mine_neighbors += 1
                     
                     self.tiles[i, j] = Tile(self.game, i, j, False, mine_neighbors)
+
+
+    def loss(self):
+        self.gamewon = False
+        for tile in self.tiles:
+            if self.tiles[tile].mine:
+                self.tiles[tile].config(bg='black')
+
+    def win(self):
+        self.gamewon = True
+        for tile in self.tiles:
+            if self.tiles[tile].mine:
+                self.tiles[tile].config(text="F",bg="red")
+
+                
         
 
 
@@ -149,87 +170,119 @@ class Tile(Label):
         self.revealed = False
         self.flagged = False
         self.flagged_neighbors = 0
+        self.focus_set()
         self.bind("<Button-1>",self.reveal)
         self.bind("<Button-3>",self.flag)
+        self.bind("<Button-2>",self.flag)
+        self.bind("<Enter>",lambda event:self.flag_coordinates(event,self.row,self.column))
+        self.bind("<space>",lambda event:self.flag_place(event))
+        
+        
+
 
     def reveal(self,event=None):
-        if self.mine == True:
-            self.configure(bg="black")
-            self.revealed = True
-            messagebox.showinfo("Game Over","You Lose")
-        else:
-            if self.mine_neighbors == 0:
-                self.configure(relief=SUNKEN,bg="light gray",text=" ")
-                self.revealed = True
-                self.reveal_neighbors(self.row,self.column)
-            
-            #counting nearby mines if Tile in creation is not a mine
-            
-            
-            self.flagged_neighbors = 0
-            if self.row - 1 >= 0:
-                if (Globals.board.tiles[self.row-1,self.column].flagged == True):
-                    self.flagged_neighbors +=1
-            if self.row - 1 >= 0 and self.column - 1 >= 0:        
-                if (Globals.board.tiles[self.row-1,self.column-1].flagged == True):
-                    self.flagged_neighbors +=1
-            if self.row - 1 >= 0 and self.column + 1 < Globals.board.width:
-                if (Globals.board.tiles[self.row-1,self.column+1].flagged == True):
-                    self.flagged_neighbors +=1
-            if self.column - 1 >= 0:
-                if (Globals.board.tiles[self.row,self.column-1].flagged == True):
-                    self.flagged_neighbors +=1
-            if self.column + 1 < Globals.board.width:
-                if (Globals.board.tiles[self.row,self.column+1].flagged == True):
-                    self.flagged_neighbors +=1
-            if self.row + 1 < Globals.board.height:
-                if (Globals.board.tiles[self.row+1,self.column].flagged == True):
-                    self.flagged_neighbors +=1
-            if self.row + 1 < Globals.board.height and self.column - 1 >= 0:
-                if (Globals.board.tiles[self.row+1,self.column-1].flagged == True):
-                    self.flagged_neighbors +=1
-            if self.row + 1 < Globals.board.height and self.column + 1 < Globals.board.width:
-                if (Globals.board.tiles[self.row+1,self.column+1].flagged == True):
-                    self.flagged_neighbors +=1
-            
-            if self.flagged_neighbors == self.mine_neighbors and self.revealed == True:
-                self.reveal_neighbors(self.row,self.column)
+        if Globals.board.gamewon == None:
+            if self.flagged == False:
+                if self.mine == True:
+                    self.configure(bg="black")
+                    self.revealed = True
+                    Globals.board.loss()
+                else:
+                    if self.mine_neighbors == 0:
+                        self.configure(relief=SUNKEN,bg="light gray",text=" ")
+                        self.revealed = True
+                        self.reveal_neighbors(self.row,self.column)
+                        
+                    #counting nearby mines if Tile in creation is not a mine
+                    
+                    
+                    self.flagged_neighbors = 0
+                    if self.row - 1 >= 0:
+                        if (Globals.board.tiles[self.row-1,self.column].flagged == True):
+                            self.flagged_neighbors +=1
+                    if self.row - 1 >= 0 and self.column - 1 >= 0:        
+                        if (Globals.board.tiles[self.row-1,self.column-1].flagged == True):
+                            self.flagged_neighbors +=1
+                    if self.row - 1 >= 0 and self.column + 1 < Globals.board.width:
+                        if (Globals.board.tiles[self.row-1,self.column+1].flagged == True):
+                            self.flagged_neighbors +=1
+                    if self.column - 1 >= 0:
+                        if (Globals.board.tiles[self.row,self.column-1].flagged == True):
+                            self.flagged_neighbors +=1
+                    if self.column + 1 < Globals.board.width:
+                        if (Globals.board.tiles[self.row,self.column+1].flagged == True):
+                            self.flagged_neighbors +=1
+                    if self.row + 1 < Globals.board.height:
+                        if (Globals.board.tiles[self.row+1,self.column].flagged == True):
+                            self.flagged_neighbors +=1
+                    if self.row + 1 < Globals.board.height and self.column - 1 >= 0:
+                        if (Globals.board.tiles[self.row+1,self.column-1].flagged == True):
+                            self.flagged_neighbors +=1
+                    if self.row + 1 < Globals.board.height and self.column + 1 < Globals.board.width:
+                        if (Globals.board.tiles[self.row+1,self.column+1].flagged == True):
+                            self.flagged_neighbors +=1
+                    
+                    if self.flagged_neighbors == self.mine_neighbors and self.revealed == True:
+                        self.reveal_neighbors(self.row,self.column)
 
 
-            #reveal value of tiles
+                    #reveal value of tiles
 
-            else:
-                if self.mine_neighbors == 1:
-                    self.configure(relief=SUNKEN, bg="light gray",text=self.mine_neighbors,fg="blue")
-                    self.revealed = True
-                if self.mine_neighbors == 2:
-                    self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="green")
-                    self.revealed = True
-                if self.mine_neighbors == 3:
-                    self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="red")
-                    self.revealed = True
-                if self.mine_neighbors == 4:
-                    self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="purple")
-                    self.revealed = True
-                if self.mine_neighbors == 5:
-                    self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="maroon")
-                    self.revealed = True
-                if self.mine_neighbors == 6:
-                    self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="teal")
-                    self.revealed = True
-                if self.mine_neighbors == 7:
-                    self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="Black")
-                    self.revealed = True
-                if self.mine_neighbors == 8:    
-                    self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="gray")
-                    self.revealed = True    
+                    else:
+                        if self.mine_neighbors == 1:
+                            self.configure(relief=SUNKEN, bg="light gray",text=self.mine_neighbors,fg="blue")
+                            self.revealed = True
+                            self.checkWin()
+                        if self.mine_neighbors == 2:
+                            self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="green")
+                            self.revealed = True
+                            self.checkWin()
+                        if self.mine_neighbors == 3:
+                            self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="red")
+                            self.revealed = True
+                            self.checkWin()
+                        if self.mine_neighbors == 4:
+                            self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="purple")
+                            self.revealed = True
+                            self.checkWin()
+                        if self.mine_neighbors == 5:
+                            self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="maroon")
+                            self.revealed = True
+                            self.checkWin()
+                        if self.mine_neighbors == 6:
+                            self.checkWin()
+                            self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="teal")
+                            self.revealed = True
+                            self.checkWin()
+                        if self.mine_neighbors == 7:
+                            self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="Black")
+                            self.revealed = True
+                            self.checkWin()
+                        if self.mine_neighbors == 8:    
+                            self.configure(relief=SUNKEN,bg="light gray",text=self.mine_neighbors,fg="gray")
+                            self.revealed = True  
+                            self.checkWin()
+
                
+    def flag_coordinates(self,event,i,j):
+        Globals.y = i
+        Globals.x = j
+
+
+    def flag_place(self,event):
+        if Globals.board.tiles[Globals.y,Globals.x].flagged == False and Globals.board.tiles[Globals.y,Globals.x].revealed == False:
+            Globals.board.tiles[Globals.y,Globals.x].configure(bg="red", text="F")
+            Globals.board.tiles[Globals.y,Globals.x].flagged = True
+        elif Globals.board.tiles[Globals.y,Globals.x].revealed == False:
+            Globals.board.tiles[Globals.y,Globals.x].configure(bg="white",text="")
+            Globals.board.tiles[Globals.y,Globals.x].flagged = False
+
 
     def flag(self,event):
         if self.flagged == False and self.revealed == False:
             self.configure(bg="red", text="F")
             self.flagged = True
-        else:
+        elif self.revealed == False:
             self.configure(bg="white",text="")
             self.flagged = False
     
@@ -266,6 +319,16 @@ class Tile(Label):
             if Globals.board.tiles[row+1,column+1].revealed == False:
                 if Globals.board.tiles[row+1,column+1].flagged == False:
                     Globals.board.tiles[row+1,column+1].reveal()
+
+    def checkWin(self):
+        total_mines = Globals.board.mines
+        total_tiles = Globals.width * Globals.height
+        total_non_mines = total_tiles-total_mines
+        for tile in Globals.board.tiles:
+            if Globals.board.tiles[tile].revealed:
+                total_non_mines -= 1
+        if total_non_mines == 0:
+            Globals.board.win()
     
 
 
@@ -273,6 +336,7 @@ def beginner_clicked():
     Globals.height = 10
     Globals.width = 10
     Globals.mines = 10
+    Globals.board.gamewon = None
     Globals.board.update_board(Globals.game_frame,Globals.height,Globals.width,Globals.mines)
     
     
@@ -284,6 +348,7 @@ def intermediate_clicked():
     Globals.height = 16
     Globals.width = 16
     Globals.mines = 40
+    Globals.board.gamewon = None
     Globals.board.update_board(Globals.game_frame,Globals.height,Globals.width,Globals.mines)
     
     
@@ -296,6 +361,7 @@ def expert_clicked():
     Globals.height = 16
     Globals.width = 30
     Globals.mines = 99
+    Globals.board.gamewon = None
     Globals.board.update_board(Globals.game_frame,Globals.height,Globals.width,Globals.mines)
     
 
@@ -305,9 +371,11 @@ def custom_clicked():
     Globals.height = 30
     Globals.width = 30
     Globals.mines = 150
+    Globals.board.gamewon = None
     Globals.board.update_board(Globals.game_frame,Globals.height,Globals.width,Globals.mines)
 
 def restart_clicked():
+    Globals.board.gamewon = None
     Globals.board.update_board(Globals.game_frame,Globals.height,Globals.width,Globals.mines)
 
     
